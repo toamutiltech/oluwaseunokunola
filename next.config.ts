09f1e1489ac165ba38e442b274343f43a4402793
Oluwaseun Adeolu Okunola <oluwaseunokunola@gmail.com>
2026-01-29 15:47:46 +0100

    update on my cv

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-// next.config.js
-module.exports = {
   output: 'export',
 };
-
 
 export default nextConfig;
